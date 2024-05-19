5fc50a5039a2ad21746289a4abbb1977e7e0e61f
singh-jp-7 <ws.com540@gmail.com>
2024-05-19 16:31:28 +0530

    Initial Start Commit

---
+++ App.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client"
+
+const AppBody = () => <h1>Hello There</h1>
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(<AppBody/>)
